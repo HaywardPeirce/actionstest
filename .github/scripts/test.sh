0508f4dae826357464d40a07e045df7ec9f85ce8
Hayward Peirce <HaywardPeirce@users.noreply.github.com>
2022-03-15 16:01:50 -0700

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,1 +1,17 @@
 echo "working bash script"
+
+echo ${FILE_EXTENSION}
+
+printenv FILE_EXTENSION
+
+for FILE in *; do echo $FILE; done
+
+for FILE in *; do
+  if grep -q SomeString "$File"; then
+    echo "$FILE matches string"
+  fi
+done
+
+# if grep -Fxq "string" file.txt; then #do some code...#; 
+
+# fi
